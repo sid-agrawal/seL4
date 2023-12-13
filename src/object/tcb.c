35b5fe36f06e3dce3f0c92e836a32109e854b776
Krishnan Winter <krishnanwinter1@gmail.com>
2023-12-13 13:53:34 +1100

    Added whitespace back into tcb.c

--- src/object/tcb.c
+++ src/object/tcb.c
@@ -282,6 +282,7 @@ void tcbReleaseEnqueue(tcb_t *tcb)
 {
     assert(thread_state_get_tcbInReleaseQueue(tcb->tcbState) == false);
     assert(thread_state_get_tcbQueued(tcb->tcbState) == false);
+
     tcb_t *before = NULL;
     tcb_t *after = NODE_STATE_ON_CORE(ksReleaseHead, tcb->tcbAffinity);
 
